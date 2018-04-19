4ce80e84641a385e4c346a85afd50cabbb811f94
Vincent Howard <vincenguyenhoward@gmail.com>
2018-04-19 17:58:58 -0600

    responsiveness is finished. The navigation bar into hamburger menu is left

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -5,19 +5,23 @@ export default class NavigationBar extends Component {
   render() {
     return (
 <div className='nav-background'>
-  <div className='nav-container'>
+<div className='main-nav'>
+  <div className='squeeze'>
+  <div>
     <div className='nav-flex'>
       <ul>
         <li className='nav-title'>IMPAV FOUNDATIONS</li>
       </ul>
       <div className='nav-align-right'>
         <div className='nav-flex-right'>
-          <div className='ion-social-github' href="https://github.com/vinnyhoward"></div>
-          <div className='ion-social-linkedin' href="https://www.linkedin.com/in/vinnyhoward/"></div>
-          <div className='ion-social-instagram-outline' href="https://www.instagram.com/vinnyvader/"></div>
+          <a href="https://github.com/vinnyhoward"><div className='ion-social-github'></div></a>
+          <a href="https://www.linkedin.com/in/vinnyhoward/"><div className='ion-social-linkedin'></div></a>
+          <a href="https://www.instagram.com/vinnyvader/"><div className='ion-social-instagram-outline'></div></a>
         </div>
       </div>
     </div>
+    </div>
+  </div>
   </div>
 </div>
     )
--- src/components/subHeader/SubHeader.js
+++ src/components/subHeader/SubHeader.js
@@ -7,9 +7,9 @@ class SubHeader extends Component {
   render() {
     return (
       <div className='sub-container'>
-        <img className={ 'secondary-asset' + ' ' + this.props.animation } src={this.props.subContent} alt="sub-content"/>
+        <img className={ 'secondary-asset ' + this.props.animation } src={this.props.subContent} alt="sub-content"/>
           <div className='sub-header__margins'> 
-          <div className={'sub-header__padding' + ' ' + this.props.padding }>
+          <div className={'sub-header__padding ' + this.props.padding }>
       <div className='sub-header-title'>{this.props.title}</div>
             <hr className='horizontal-line' />
             <div className='sub-header__caption'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi optio doloribus eaque impedit alias harum eveniet, odio nostrum aut dolorem.</div>
