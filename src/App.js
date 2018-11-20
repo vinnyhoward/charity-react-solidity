3d21eb95dc4b7b131f9a99b172fdad8225577357
Vince Howard <vincenguyenhoward@gmail.com>
2018-11-19 21:59:21 -0700

    removed annoying white curve that wouldn't be responsive

--- src/App.js
+++ src/App.js
@@ -11,46 +11,32 @@ const park = require('./images/tree-iso.png');
 const cannedGood = require('./images/can-iso.png');
 
 class App extends Component {
-  render() {
-    return (
-<Layout>
- <NavigationBar />
- <svg id="waves" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1600 222.73">
+	render() {
+		return (
+			<Layout>
+				<NavigationBar />
+				{/* <svg id="waves" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1600 222.73">
     <title>waves</title>
     <path className="cls-1" d="M0,109S315,213,553,213,1191-21,1600,48V258H0Z" transform="translate(0 -35.27)" />
-  </svg>
-      <Banner />
-  <div className='main'>
-  
-    <div className='squeeze'>
-    <div className='sub-container-flex'>
-    <SubHeader
-    padding='extra-padding'
-    title='Food'
-    subContent={cannedGood}
-    animation='reg'
-    />
-    <SubHeader
-    title='Education'
-    subContent={book}
-    animation='medium'
-    />
-    <SubHeader
-    title='Recreation'
-    subContent={park}
-    animation='slow'
-    />
-    </div>
-    <div className='message-section-title'>Donors</div>
-    <div className='message-flex'>
-    <MessageListContainer />
-    {/* <div className='round-circle'></div> */}
-    </div>
-    </div>
-  </div>
-</Layout>
-    );
-  }
+  </svg> */}
+				<Banner />
+				<div className="main">
+					<div className="squeeze">
+						<div className="sub-container-flex">
+							<SubHeader padding="extra-padding" title="Food" subContent={cannedGood} animation="reg" />
+							<SubHeader title="Education" subContent={book} animation="medium" />
+							<SubHeader title="Recreation" subContent={park} animation="slow" />
+						</div>
+						<div className="message-section-title">Donors</div>
+						<div className="message-flex">
+							<MessageListContainer />
+							{/* <div className='round-circle'></div> */}
+						</div>
+					</div>
+				</div>
+			</Layout>
+		);
+	}
 }
 
 export default App;
